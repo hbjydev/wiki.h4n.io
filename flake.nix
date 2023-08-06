276daa7065ab43b4626cc80fd802d08ba2b306ad
Hayden Young <22327045+hbjydev@users.noreply.github.com>
2023-08-06 13:15:47 +0100

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "wiki.h4n.io nix flake";
+
+  inputs = {
+    devenv.url = "github:cachix/devenv";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ inputs.devenv.flakeModule ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }:
+      {
+        devenv.shells.default = {
+          name = "wiki";
+          packages = with pkgs; [ just ];
+          languages.python = {
+            enable = true;
+            venv.enable = true;
+          };
+        };
+      };
+    };
+}
